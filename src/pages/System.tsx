91e634d83555ab8a51aade8dd6119d8048825b0c
RafaelAlmeida00 <rafaelalmeid00@gmail.com>
2025-07-02 18:14:26 -0300

    a

--- src/pages/System.tsx
+++ src/pages/System.tsx
@@ -1,11 +1,36 @@
-import { Box } from "@chakra-ui/react";
-import Sidebar from "../components/app/Drawer";
+import {
+    Box
+} from "@chakra-ui/react";
 import { Outlet } from "react-router";
+import Sidebar from "../components/app/Drawer";
+import { useEffect, useState } from "react";
 
 export default function System() {
+    const [isMobileDevice, setIsMobileDevice] = useState(false);
+
+    useEffect(() => {
+        function useIsMobileDevice() {
+            const [isMobileDevice, setIsMobileDevice] = useState(false);
+
+            useEffect(() => {
+                const userAgent = navigator.userAgent || navigator.vendor;
+
+                const isMobile =
+                    /android|iphone|ipad|ipod|opera mini|iemobile|mobile/i.test(userAgent);
+
+                setIsMobileDevice(isMobile);
+            }, []);
+
+            return isMobileDevice;
+        }
+        setIsMobileDevice(useIsMobileDevice());
+    }, [isMobileDevice]);
+
+
+
     return (
         <>
-            <Sidebar />
+            {!isMobileDevice && <Sidebar />}
             <Box>
                 <Outlet />
             </Box>
